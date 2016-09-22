8ba2e444e87e1773820cf64100451643900a0886
jia <cnexcelee@gmail.com>
2016-09-22 18:02:34 +0800

    修改bug - 滑动到最后一个界面继续滑动，如果up位置在图片外区域，就返回到上一个节目的bug

--- app/src/main/java/com/hhl/tubatu/ClipViewPager.java
+++ app/src/main/java/com/hhl/tubatu/ClipViewPager.java
@@ -11,6 +11,11 @@ import android.view.View;
  */
 public class ClipViewPager extends ViewPager {
 
+    //默认距离
+    private final static float DISTANCE = 10;
+    private float downX;
+    private float downY;
+
     public ClipViewPager(Context context) {
         super(context);
     }
@@ -21,8 +26,18 @@ public class ClipViewPager extends ViewPager {
 
     @Override
     public boolean dispatchTouchEvent(MotionEvent ev) {
+        if(ev.getAction() == MotionEvent.ACTION_DOWN){
+            downX = ev.getX();
+            downY = ev.getY();
+        }else if (ev.getAction() == MotionEvent.ACTION_UP) {
 
-        if (ev.getAction() == MotionEvent.ACTION_UP) {
+            float upX = ev.getX();
+            float upY = ev.getY();
+            //如果 up的位置和down 的位置 距离 > 设置的距离,则事件继续传递,不执行下面的点击切换事件
+            if(Math.abs(upX - downX) > DISTANCE || Math.abs(upY - downY) > DISTANCE){
+                return super.dispatchTouchEvent(ev);
+            }
+
             View view = viewOfClickOnScreen(ev);
             if (view != null) {
                 int index = indexOfChild(view);
